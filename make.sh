94e14141273b3981b2892e1cf51638085059cd46
Abdeali Bhagat <abdealibhagat787@gmail.com>
2018-03-23 14:32:49 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -7,33 +7,33 @@ bash_rc_path="./dotfiles/.bashrc"
 this=$(pwd)
 old="old_dotfiles"
 
-echo "Finding the files"
+echo "Checking the Files in the Repository"
 
 if [ $(find . -name $bash_rc) = $bash_rc_path ] && [ $(find . -name $bash_profile) = $bash_profile_path ]
 then
-	echo "It is present"
+	echo "Files Found"
 else
-	echo "It is not present"
+	echo "Missing Files $bash_rc or $bash_profile"
 fi
 
-echo "Finding Home Directory"
+echo "Setting Up the Home Directory"
 
 if [ -n "$(find $HOME -name .bashrc -o -name .bash_profile | head -n 2)" ];
 then
 	echo $this
 	mkdir "$HOME/old_dotfiles"
-	echo "old_dotfiles Directory Made"
+	echo "$old Directory Made"
 	cd "$this/dotfiles/"
 	echo "Moving Existing Dotfiles to $old directory"
-#	mv $HOME/.bashrc $HOME/old_dotfiles/
-#	mv $HOME/.profile $HOME/old_dotfiles/
+	mv $HOME/.bashrc $HOME/old_dotfiles/
+	mv $HOME/.profile $HOME/old_dotfiles/
 	echo "Files Moved"
 	echo "Moving new dotfiles in the $HOME Directory"
 	echo $this
-	cp $bash_rc $HOME/
-	cp $bash_profile $HOME/
+	mv $bash_rc $HOME/
+	mv $bash_profile $HOME/
 	echo "Files Moved"
 
 else
-	echo "It is Not there"
+	echo "Files Not Moved"
 fi
