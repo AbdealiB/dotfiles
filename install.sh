957b6d4cc008f9b8f007e71577323369888958c7
AbdealiB <abdealibhagat787@gmail.com>
2018-03-26 22:38:06 +0530

    Adding install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+# Useful Installations
+
+echo "Installing packages .."
+
+# Asking for admin password
+sudo -v
+
+
+sudo apt-get -qq install git
+sudo apt-get -qq install curl
+sudo apt-get -qq install tree
+sudo apt-get -qq install vim
+
+echo "...done"
+echo "Packages Installed.."
+
